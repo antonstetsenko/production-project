e19d82b324ac1277ad2633a54af65eea8670b795
antonstetsenko94@gmail.com <anton2281488@mail.ru>
2023-09-12 13:59:12 +0300

    roter config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 
 import './styles/index.scss';
+import { AppRouter } from "./providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +13,7 @@ const App = () => {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage/>}/>
-          <Route path={'/'} element={<MainPage/>}/>
-        </Routes>
-      </Suspense>
+      <AppRouter/>
     </div>
   )
 }
