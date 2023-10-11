4f16139b8a395035ec1cb2b44a9f27b1217c8c3e
antonstetsenko94@gmail.com <anton2281488@mail.ru>
2023-10-11 12:04:55 +0300

    add langswitcher i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,20 +2,23 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
 
 import './styles/index.scss';
-import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar/>
-      <div className="content-page">
-        <Sidebar/>
-        <AppRouter/>
-      </div>
+      <Suspense fallback="">
+        <Navbar/>
+        <div className="content-page">
+          <Sidebar/>
+          <AppRouter/>
+        </div>
+      </Suspense>
     </div>
   )
 }
